c000725e1ccff19073a9b4fa198daf66889d5969
Maxim <doretti.female@gmail.com>
2022-06-17 01:58:11 +0500

    cors + body-parser

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, { Application } from 'express'
 import { config } from 'dotenv'
+import cors from 'cors'
+import bodyParser from 'body-parser'
 config()
 
 const PORT: number = +(process.env.PORT || 4000)
 
 const app: Application = express()
+
+app.use(cors())
+app.use(bodyParser.json())
 
 app.listen(PORT, () => {
     console.log('🔥 Server has been started on port:', PORT);
